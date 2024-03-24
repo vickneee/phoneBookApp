3285cc1b3122a67f0c26f9038161ea868b1d90da
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-03-24 17:44:27 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 const express = require('express')
-const app = express()
+const dotenv = require('dotenv')
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 // Middlewares
+dotenv.config() // Middleware to load environment variables from a .env file into process.env
+
+let app = express() // Create an express app
+
 app.use(morgan('tiny')) // Middleware to log the request details
-
 app.use(express.json()) // Middleware to parse JSON data in the body of the request
-
 app.use(cors()) // Middleware to allow requests from other origins
+// app.use(express.static('build')) // Middleware to serve static files
 
 // Data
 let persons =  [
@@ -33,6 +37,17 @@ let persons =  [
     "number": "39-23-6423122",
   }
 ]
+
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 // Routes
 // Route to get the home page
@@ -108,6 +123,18 @@ app.get('/api/info', (request, response) => {
   `;
   response.send(info);
 });
+
+const url = process.env.MONGODB_URI
+
+// console.log('connecting to', url) // Uncomment this line to see the value of the url variable
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
