4180b44d2c6363e0a8e87984362e35ef522631e9
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-03-24 21:07:57 +0200

    Space correction

--- frontend/src/components/Persons.jsx
+++ frontend/src/components/Persons.jsx
@@ -3,7 +3,7 @@ const Persons = ({persons, handleDelete}) => {
     <div>
       {persons.map((person) => (
         <p key={person.id}>
-          {person.name} {person.number}
+          {person.name} {person.number}<span> </span>
           <button onClick={() => handleDelete(person.id)}>Delete</button>
         </p>
       ))}
